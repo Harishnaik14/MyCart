39df2317cd270e8261d4892a06342e3e945154b9
HARISH NAIK <harishnaikk14@gmail.com>
2025-11-23 19:14:25 +0530

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,14 +132,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
@@ -151,5 +153,6 @@ LOGIN_URL = 'login'
 # api key for sending otp
 
 FAST2SMS_API_KEY = "T59plXn3fUSc1GvWy0uxOLmhe8BodgKwZaiMsR6qtrJC7kHAVIkExa8tiMfKV52uehNogLlXWYCOHRZm"
+
 
 
